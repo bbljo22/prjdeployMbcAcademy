f092c43cd1cef04e1526f7a2d9cc79fdb0f1f2bc
bbljo22 <bbljo2@yahoo.com>
2024-05-31 15:55:43 +0900

    패키지 중복

--- src/main/java/com/prj/config/WebSocketConfig.java
+++ src/main/java/com/prj/config/WebSocketConfig.java
@@ -1,7 +1,5 @@
 package com.prj.config;
 
-package com.prj.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
